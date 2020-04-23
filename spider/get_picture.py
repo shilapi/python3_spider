3ac5665cede2d244c7d0afd2ec0420eed8aac997
shilapi <shilapi@outlook.com>
2020-04-23 16:59:40 +0800

    图片调取基本完成，下载模块报错

---
+++ spider/get_picture.py
@@ -0,0 +1,60 @@
+#https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0
+import requests
+import urllib
+import os
+from bs4 import BeautifulSoup
+
+def findtarget (s,head,end,headurl,endurl) :#筛选出需要的内容并加上序号，存为列表
+    if isinstance(s,list) :
+        for t in range(len(s)):
+            l = str(s[t])
+            dir = './picdownload'#下载文件夹
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            name=''
+            urlpic=''
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+                }
+            headnum = l.find(head) + len(head)
+            endnum = l.find(end)
+            headurlnum = l.find(headurl) + len(headurl)
+            endurlnum = l.find(endurl)
+            name=str(l[headnum:endnum])
+            urlpic=str(l[headurlnum:endurlnum])
+            urllib.request.urlretrieve(urlpic,dir)
+            #response = requests.get(urlpic)
+            #image = Image.open(BytesIO(response.content))
+            #image.save(dir)
+            #img=requests.get(urlpic,headers=headers)
+            #imgname = name + '.png'
+            #filename = os.path.join(dir, imgname)
+            #open(bytes(filename, encoding = "utf8"), 'wb').write(bytes(urlpic, encoding = "utf8"))
+
+#爬取的链接地址
+url = "https://www.jdlingyu.mobi/collection/meizitu"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+}
+
+indexurl = requests.get(url,headers=headers)
+strtext = indexurl.text
+#print(strtext)
+soup = BeautifulSoup(strtext,'lxml')
+
+data = soup.select('#main > div.grid-bor > div > div > div.thumb.pos-r > div')#获取指定字符串，保存为list
+print(data)
+urlhead = '''
+style="background-image:url('
+'''
+urlend = '''
+')>
+'''
+head = '''
+style="background-image:url('
+'''
+end = '''
+')>
+'''
+result = findtarget(data,'head','end','urlhead','urlend')
+#print('\n'.join(result))
